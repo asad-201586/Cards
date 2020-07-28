e2af6c3208528e93e475e32f4d7a61248b1ea19f
asad-201586 <asadul201586@gmail.com>
2020-07-29 05:47:10 +0600

    added 2nd card page

--- app/src/main/java/com/asad/cards/CardTwo.java
+++ app/src/main/java/com/asad/cards/CardTwo.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CardTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_card_two);
     }
 
     public void clickListener(View view)
     {
-        Intent intent = new Intent(MainActivity.this,CardTwo.class);
+        Intent intent = new Intent(CardTwo.this,MainActivity.class);
         startActivity(intent);
     }
 }
